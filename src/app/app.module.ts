57182e9e1cb762a0c2a4fc6f8c808f85803ad684
Gleyson Sampaio <glysns@hotmail.com>
2022-04-20 06:11:09 -0300

    criando novos arquivos e limpando a main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovimentacaoNewComponent,
+    MovimentacaoListComponent
   ],
   imports: [
     BrowserModule,
