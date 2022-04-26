8f8428443f0d8a977819ac8f1f9895e3c0e51e2d
Gleyson Sampaio <glysns@hotmail.com>
2022-04-26 08:18:06 -0300

    movimentacoes

--- src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+
 
 @Component({
   selector: 'app-movimentacao-list',
@@ -8,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class MovimentacaoListComponent implements OnInit {
   movimentacoes:any;
 
-  constructor() { }
+  constructor(private movimentacaoService: MovimentacaoService) { }
 
   ngOnInit(): void {
+    this.listMovimentacoes();
+  }
+  listMovimentacoes(): void {
+    this.movimentacaoService.list()
+      .subscribe(
+        data => {
+          this.movimentacoes = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
